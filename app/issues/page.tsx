7208f146ce3d21c0b30f2148458e2822261f4f45
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-10-16 01:53:38 +0530

    create issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const page = () => {
-  return <div>Issue Tracker</div>;
+  return (
+    <div>
+      Issue Tracker
+      <Button>Hey There</Button>
+    </div>
+  );
 };
 
 export default page;
